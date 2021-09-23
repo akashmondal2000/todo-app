ee96886f73f04beeb0e226f2178ed1e86f7715a3
akashmondal2000 <mondalakash494@gmail.com>
2021-09-23 22:20:45 +0530

    Adding todo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+
+import React, { useState } from "react";
 
 function App() {
+  const [input, setInput] = useState("");
+  const [list, setList] = useState([]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Todo App</h1>
+
+      <input
+        type="text"
+        value={input}
+        onChange={(inputChange) => setInput(inputChange.target.value)}
+        placeholder="Add some task"
+      />
+      <button onClick={() => setList([...list, input])}>Add</button>
+
+      {list.length > 0 ? list.map((item) => <p>{item}</p>) : null}
     </div>
   );
 }

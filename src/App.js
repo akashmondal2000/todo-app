153c513572b80336cd8df5be4acd5e738c127cbe
akashmondal2000 <mondalakash494@gmail.com>
2021-09-25 23:49:44 +0530

    Chnage single todo to a object

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 // import logo from './logo.svg';
 // import './App.css';
+//import React from "react";
 
 import React, { useState } from "react";
 
@@ -7,26 +8,37 @@ function App() {
   const [input, setInput] = useState("");
   const [list, setList] = useState([]);
 
+  let addTodo = ()=>{
+    const d = new Date();
+    const newDate = String(d);
+    console.log(newDate);
+
+     const obj = {
+       id: newDate,
+       title: input
+     }
+
+     setList([...list, obj])
+
+  }
+
   return (
     // <div className="App">
-      <div className="container">
+    <div className="container">
+      <h1>Todo App</h1>
 
-        <h1>Todo App</h1>
+      <input
+        className="searchBox"
+        type="text"
+        value={input}
+        onChange={(inputChange) => setInput(inputChange.target.value)}
+        placeholder="Add some task"
+      />
+      <br />
+      <button onClick={addTodo} > Add</button>
 
-        <input className = "searchBox"
-          type="text"
-          value={input}
-          onChange={(inputChange) => setInput(inputChange.target.value)}
-          placeholder="Add some task"
-        /><br/>
-        <button onClick={() => setList([...list, input])}>Add</button>
-
-        <div className="todoBox">
-
-          {list.length > 0 ? list.map((item) => <p id = "todos">{item}</p>) : null}
-
-        </div>
-      </div>
+      {list.length > 0 ? list.map((item) => <p id="todos" key={item.id}>{item.title}</p>) : null}
+    </div>
     // </div>
   );
 }

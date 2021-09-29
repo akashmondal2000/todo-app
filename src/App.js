a2635fb5e93d3359b7937da2cbfcd2a98e73f623
akashmondal2000 <mondalakash494@gmail.com>
2021-09-29 23:46:23 +0530

    Add ChekeBox

--- src/App.js
+++ src/App.js
@@ -7,30 +7,34 @@ import React, { useState } from "react";
 function App() {
   const [input, setInput] = useState("");
   const [list, setList] = useState([]);
+  
 
-  let addTodo = ()=>{
+  let addTodo = () => {
     const d = new Date(); // take a date
-    const newDate = String(d); // convart date to string 
+    const newDate = String(d); // convart date to string
     console.log(newDate);
 
-     const obj = {
-       id: newDate,
-       title: input
-     }
+    const obj = {
+      id: newDate,
+      title: input,
+      isCheked: false,
+    };
 
-     setList([...list, obj])
-
-  }
+    setList([...list, obj]);
+    setInput("");
+  };
 
   // Delete todo function
-  const deleteTodo = (id)=>{
-
+  const deleteTodo = (id) => {
     const newList = list.filter((item) => item.id !== id);
 
     setList(newList);
     //console.log(id);
+  };
 
-  }
+  // // Chake Box
+  // const chakeTodo = ()=>{
+  // }
 
   return (
     // <div className="App">
@@ -42,18 +46,35 @@ function App() {
         type="text"
         value={input}
         onChange={(inputChange) => setInput(inputChange.target.value)}
-        placeholder="Add some task"
-      />
+        placeholder="Add some task" />
       <br />
-      <button id="addButton" onClick={addTodo} > Add</button>
 
-      {list.length > 0 ? list.map((item) => <p id="todos" key={item.id}>{item.title} 
-      
-      <button type='button' onClick = {()=>deleteTodo(item.id)}>Delete</button>
-      </p>) : null}
+      <button id="addButton" onClick={addTodo}>
+        {" "}
+        Add
+      </button>
+
+      {list.length > 0
+        ? list.map((item) => (
+            <div key={item.id}>
+              <p id="todos" >
+                {item.title}
+              </p>
+
+              <button type="button" onClick={() => deleteTodo(item.id)}>Delete</button>
+
+              <input type="checkbox" value ={item.isCheked} onChange = {()=>{ 
+              console.log(item.title, " : check status > ", !item.isCheked);
+              setList(
+              list.map(i=> i.id === item.id ? {...i, isCheked: !i.isCheked} : i  )
+              )}
+              } />
+                  
+            </div>
+          ))
+        : null}
     </div>
     // </div>
-    
   );
 }
 

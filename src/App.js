021a2b56c4888fb8c005e12aab2fb8330bf8082e
akashmondal2000 <mondalakash494@gmail.com>
2021-10-03 17:23:27 +0530

    Add EditTodo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-
 import React, { useState } from "react";
 
 function App() {
   const [input, setInput] = useState("");
   const [list, setList] = useState([]);
-  
+  const [editinput, setEditinput] = useState("");
 
   let addTodo = () => {
     const d = new Date(); // take a date
@@ -15,7 +14,7 @@ function App() {
       id: newDate,
       title: input,
       isCheked: false,
-      isEdit:false,
+      isEdit: false,
     };
 
     setList([...list, obj]);
@@ -30,7 +29,14 @@ function App() {
     //console.log(id);
   };
 
-  const editTodo = ()=>{}
+  const editTodo = (todo) => {
+    setList(
+      list.map((i) =>
+        i.id === todo.id ? { ...i, title: editinput, isEdit: !i.isEdit } : i
+      )
+    );
+    setEditinput("")
+  };
 
   // // Chake Box
   // const chakeTodo = ()=>{
@@ -46,7 +52,8 @@ function App() {
         type="text"
         value={input}
         onChange={(inputChange) => setInput(inputChange.target.value)}
-        placeholder="Add some task" />
+        placeholder="Add some task"
+      />
       <br />
 
       <button id="addButton" onClick={addTodo}>
@@ -57,42 +64,69 @@ function App() {
       {list.length > 0
         ? list.map((item) => (
             <div className="todoList" key={item.id}>
+              {item.isEdit ? (
+                <>
+                  <input
+                    type="text"
+                    value={editinput}
+                    onChange={(e) => setEditinput(e.target.value)}
+                    placeholder="Edit"
+                  />
 
-            {item.isEdit? 
-            <>
-            <input  />
-            <button>save
-            </button>
-            </>
-            :
-            
-            <>
-            <input className="listStyle" type="checkbox" value ={item.isCheked} onChange = {()=>{ 
-              console.log(item.title, " : check status > ", !item.isCheked);
-              setList(
-              list.map(i=> i.id === item.id ? {...i, isCheked: !i.isCheked} : i  )
+                  <button type="button" onClick={() => editTodo(item)}>
+                    save
+                  </button>
+                </>
+              ) : (
+                <>
+                  <input
+                    className="listStyle"
+                    type="checkbox"
+                    value={item.isCheked}
+                    onChange={() => {
+                      console.log(
+                        item.title,
+                        " : check status > ",
+                        !item.isCheked
+                      );
+                      setList(
+                        list.map((i) =>
+                          i.id === item.id ? { ...i, isCheked: !i.isCheked } : i
+                        )
+                      );
+                    }}
+                  />
+
+                  <p id="todoText">{item.title}</p>
+
+                  <button
+                    className="listStyle"
+                    type="button"
+                    onClick={() => deleteTodo(item.id)}
+                  >
+                    Delete
+                  </button>
+
+                  <button
+                    type="button"
+                    onClick={() => {
+                      setEditinput(item.title);
+
+                      setList(
+                        list.map((ed) =>
+                          ed.id === item.id ? { ...ed, isEdit: !ed.isEdit } : ed
+                        )
+                      );
+                    }}
+                  >
+                    Edit
+                  </button>
+                </>
               )}
-              }/> 
-
-              <p id="todoText">
-                {item.title}
-              </p>
-
-              <button className="listStyle" type="button" onClick={() => deleteTodo(item.id)}>Delete</button>
-
-              <button type="button" onClick = {()=>{
-
-                setList(
-                  list.map(ed=> ed.id === item.id ? {...ed,isEdit : !ed.isEdit} : ed)
-                )
-              }}>Edit</button>
-            </>
-            }
             </div>
           ))
         : null}
     </div>
-   
   );
 }
 
